d92a123d36577d96957550df4499437874f339c3
Anna <ajimenea@cs.utexas.edu>
2024-02-03 17:01:54 -0600

    Update navbar

--- my-app/src/components/MainNavbar.js
+++ my-app/src/components/MainNavbar.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
- import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
- import NavDropdown from 'react-bootstrap/NavDropdown';
-import {LinkContainer} from 'react-router-bootstrap';
- import { Link } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
+import '../style.css';
 
+import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function MainNavbar() {
-  
-    return (
-        <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary" >
-          <Container>
-            <LinkContainer to="/">
-              <Navbar.Brand >Syrian Refugee Crisis</Navbar.Brand>
-            </LinkContainer>
-
-          
-          
-
-          </Container>
-        </Navbar>
-      );
+  return (
+    <Navbar expand="lg" id='Nbar'>
+      <Container>
+        <Navbar href="#home" className="Navbar_text" >Barbie's Fashion Forecast</Navbar>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default MainNavbar;
