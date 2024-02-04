531e18607c37978251d053e4a89d82a1777b73ec
Yingting Cao <nicolec@cs.utexas.edu>
2024-02-04 05:03:47 -0600

    fetch returns null needs fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import MainNavbar from './components/MainNavbar.js';
 import'./components/MainNavbar.js';
 // import Navbar from './components/MainNavbar.js';
 
 function App() {
+
+
+  const [data, setdata] = useState([{}])
+
+  useEffect(() => {
+      fetch("/members").then(
+        res => res.jeon()).then(
+        data => {
+            setdata(data)
+            console.log(data)
+        }
+      )
+  }, [])
   return (
 
     <div className="App">
@@ -14,3 +27,4 @@ function App() {
 }
 
 export default App;
+
